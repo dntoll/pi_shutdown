3ece78bcaf9768fc6a7d243ca95e5f6825e0c82d
dntoll <daniel@familjentoll.se>
2015-01-12 20:26:49 +0000

    new version

--- pi_shutdown.sh
+++ pi_shutdown.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo shutdown on button press. 
 echo waiting...
-
-gpio wfi 7 falling
+/usr/local/bin/gpio mode 1 out
+/usr/local/bin/gpio write 1 1
+/usr/local/bin/gpio wfi 7 falling
 echo "shutdown in progress" | wall
 
+halt
 
-echo done.
