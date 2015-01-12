e8a3a55fed437005d18b5b5ddaa98336c2cad5f9
dntoll <daniel@familjentoll.se>
2015-01-12 20:30:32 +0000

    comments

--- pi_shutdown.sh
+++ pi_shutdown.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-echo shutdown on button press. 
-echo waiting...
+
+
+#turn on LED connected to wiring pin 1
 /usr/local/bin/gpio mode 1 out
 /usr/local/bin/gpio write 1 1
+
+#wait for button press on wiring pin 7
 /usr/local/bin/gpio wfi 7 falling
+
+#broadcast message to all connected
 echo "shutdown in progress" | wall
 
+#shut down the pi
 halt
+#the LED will go out when its safe to turn off power
 
