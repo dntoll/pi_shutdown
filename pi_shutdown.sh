ea3e438057e34c22aa8a85ad4e77969a515a73a7
dntoll <daniel@familjentoll.se>
2015-01-17 17:21:46 +0000

    changed port to port 5

--- pi_shutdown.sh
+++ pi_shutdown.sh
@@ -2,11 +2,11 @@
 
 
 #turn on LED connected to wiring pin 1
-/usr/local/bin/gpio mode 1 out
-/usr/local/bin/gpio write 1 1
+/usr/local/bin/gpio mode 18 out
+/usr/local/bin/gpio write 18 1
 
 #wait for button press on wiring pin 7
-/usr/local/bin/gpio wfi 7 falling
+/usr/local/bin/gpio wfi 20 rising
 
 #broadcast message to all connected
 echo "shutdown in progress" | wall
